6ea261b0efc258988ed8cf5b7fe0f1a68dad24a7
한민규 <minku0128@naver.com>
2024-05-10 23:15:47 +0900

    오류수정

--- common/forms.py
+++ common/forms.py
@@ -7,7 +7,7 @@ class UserForm(UserCreationForm):
     
     # 이메일 등의 속성을 추가하기 위해서는 UserCreationForm 클래스를 상속하여 만들어야 한다.
     email = forms.EmailField(label="이메일")
-    korailID = forms.TextInput(label="korailID")
+    korailID = forms.CharField(label="korailID")
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email", "korailID")
